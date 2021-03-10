cb315e3df0585abd24b81eb0a76a750d301b8a16
MarkusThielker <mail.markus.thielker@gmail.com>
2021-03-10 01:36:25 +0100

    fix: added comments and implemented fixed process exit, stopping all threads.

--- server/src/main/kotlin/de/markus_thielker/streamplus/server/RunServer.kt
+++ server/src/main/kotlin/de/markus_thielker/streamplus/server/RunServer.kt
@@ -2,30 +2,44 @@ package de.markus_thielker.streamplus.server
 
 import java.net.ServerSocket
 import kotlin.concurrent.thread
+import kotlin.system.exitProcess
 
 fun main() {
 
+    // open server socket
     val serverSocket = ServerSocket(5050)
 
+    // set running var for exit
     var running = true
 
+    // open new thread accepting and managing connection requests
     thread {
         while (running) {
+
+            // accept and route connections to new thread
             val client = serverSocket.accept()
             thread { ConnectionHandler(client = client) }
         }
     }
 
+    // read server commands in main thread
     while (running) {
 
+        print("Command: ")
+
         when(readLine()) {
+
+            // stop server for command "exit"
             "exit" -> {
                 println("Stopping Server...")
                 running = false
             }
+
+            // else -> return that input didn't match any valid command
             else -> { println("Unknown command! - Type \"exit\" to stop the server") }
         }
     }
 
-    println("Server stopped.")
+    // stops all running threads
+    exitProcess(0)
 }
